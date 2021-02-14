f78e734905559bbd90330b6f1f6c836b52ca7a6b
jkate8 <yura.fedoruk@gmail.com>
2021-02-14 23:58:24 +0300

    First commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Test test = new Test(5);
+        System.out.println(test.getX());
     }
 }
